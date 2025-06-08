238dcacdcefae9dbe4c83f04727ca15bdc2288bd
prabhu varma cherukuri <prabhuvarma368@gmail.com>
2025-06-07 20:21:07 -0500

    Fix Terraform Formatting

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"  # More recent and compatible with current GCP features
+      version = "~> 5.0" # More recent and compatible with current GCP features
     }
   }
 }
